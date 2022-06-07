24edc7c68f28b79c741f480da6fce240dc14afbd
Sunil D S <sunil.d.shiv@gmail.com>
2022-06-07 20:22:22 +0530

    Add Restrofit API and modified follwing module :
    - Added specific packages for model class.
    - Added new fragments and related layout files.

---
+++ app/src/main/java/com/recipes/foody/RemoteDataSource.kt
@@ -0,0 +1,4 @@
+package com.recipes.foody
+
+class RemoteDataSource() {
+}
--- app/src/main/java/com/recipes/foody/ui/fragments/foodjoke/FoodJokeFragment.kt
+++ app/src/main/java/com/recipes/foody/ui/fragments/foodjoke/FoodJokeFragment.kt
@@ -1,10 +1,11 @@
-package com.recipes.foody
+package com.recipes.foody.ui.fragments.foodjoke
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.recipes.foody.R
 
 class FoodJokeFragment : Fragment() {
 
